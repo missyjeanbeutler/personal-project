7b49ed0d8da373c90cccadaa9ef34a8cf5700198
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-03-13 21:32:16 -0600

    messy but progress

--- www/views/3-trail-data/dataCtrl.js
+++ www/views/3-trail-data/dataCtrl.js
@@ -1,4 +1,4 @@
-angular.module('trailsApp').controller('dataCtrl', function($scope, $stateParams, mainSvc, postSvc) {
+angular.module('trailsApp').controller('dataCtrl', function($scope, $stateParams, mainSvc, postSvc, NgMap) {
 
     trailData($stateParams.id)
 
@@ -6,8 +6,29 @@ angular.module('trailsApp').controller('dataCtrl', function($scope, $stateParams
         mainSvc.trailData(id).then(response => {
             response.time = Math.round((response.time / 60) * 10) / 10;
             $scope.trail = response;
+            response.coords = path(JSON.parse(response.coords))
+
+            $scope.center = response.coords[0][0] + ',' + response.coords[0][1];
         })
     }
+
+    $scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAVCXggIslnObF-rgqIyZzScfscjv_-o8I"
+
+    NgMap.getMap().then(function (map) {
+        console.log(map.getCenter());
+        console.log('markers', map.markers);
+        console.log('shapes', map.shapes);
+
+    })
+
+
+    function path(coords) {
+            let fixedLatLong = []
+            for (let i = 0; i < coords.length; i++) {
+                fixedLatLong.push([coords[i][1], coords[i][0]])
+            }
+            return fixedLatLong;
+        }
 
     
 
--- www/views/3-trail-data/mapDirective.js
+++ www/views/3-trail-data/mapDirective.js
@@ -4,7 +4,15 @@ angular.module('trailsApp')
             restrict: 'E',
             templateUrl: './views/3-trail-data/map.html',
             controller: function ($scope) {
-                $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyAVCXggIslnObF-rgqIyZzScfscjv_-o8I"
+                // $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyAVCXggIslnObF-rgqIyZzScfscjv_-o8I"
+                var map;
+      function initMap() {
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.7413549, lng: -73.9980244},
+          zoom: 13
+        });
+      }
 
             }
         }
