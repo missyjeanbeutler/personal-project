ab3707c4f8b45c77653413fa6d32eb21bc2a82b2
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-03-22 19:27:40 -0600

    home page with fadein animations

--- www/views/1-home/homeCtrl.js
+++ www/views/1-home/homeCtrl.js
@@ -1,7 +1,28 @@
 angular.module('trailsApp').controller('homeCtrl', function () {
 
 
-//------------parallax scrolling effect----------//
+    //--------------loading animation---------------//
+
+    function onReady(callback) {
+        var intervalID = window.setInterval(checkReady, 1000);
+        function checkReady() {
+            if (document.getElementsByTagName('body')[0] !== undefined) {
+                window.clearInterval(intervalID);
+                callback.call(this);
+            }
+        }
+    }
+
+    function show(id, value) {
+        document.getElementById(id).style.display = value ? 'block' : 'none';
+    }
+
+    onReady(function () {
+        show('loading', false);
+    });
+
+
+    //------------parallax scrolling effect----------//
 
     (function () {
         var parallax = document.querySelectorAll(".parallax"),
@@ -14,6 +35,54 @@ angular.module('trailsApp').controller('homeCtrl', function () {
             });
         };
     })();
+
+    //---------------fade in on scroll-----------//
+
+    $(document).ready(function () {
+        // $('#home-1').css('opacity', 0);
+        // $('#home-2').css('opacity', 0);
+        $('#home-3').css('opacity', 0);
+        $('#home-4').css('opacity', 0);
+        $('#home-5').css('opacity', 0);
+        $('#home-6').css('opacity', 0);
+        $('#home-7').css('opacity', 0);
+        // $('#home-1').waypoint(function () {
+        //     $('#home-1').addClass('fadeInLeft');
+        // }, {
+        //     offset: '100%'
+        // });
+        // $('#home-2').waypoint(function () {
+        //     $('#home-2').addClass('fadeInRight');
+        // }, {
+        //     offset: '100%'
+        // });
+        $('#home-3').waypoint(function () {
+            $('#home-3').addClass('fadeInLeft');
+        }, {
+            offset: '90%'
+        });
+        $('#home-4').waypoint(function () {
+            $('#home-4').addClass('fadeInRight');
+        }, {
+            offset: '90%'
+        });
+        $('#home-5').waypoint(function () {
+            $('#home-5').addClass('fadeInLeft');
+        }, {
+            offset: '90%'
+        });
+        $('#home-6').waypoint(function () {
+            $('#home-6').addClass('fadeInRight');
+        }, {
+            offset: '90%'
+        });
+        $('#home-7').waypoint(function () {
+            $('#home-7').addClass('fadeInLeft');
+        }, {
+            offset: '90%'
+        });
+
+    });
 
 
 
